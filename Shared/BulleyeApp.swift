6e07a0c34717f7dc5ae69e6371fae28a15ebc217
YJ <yjforlearning@gmail.com>
2022-12-12 17:48:28 +0800

    Initial Commit

---
+++ Shared/BulleyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulleyeApp.swift
+//  Shared
+//
+//  Created by yujiejhang on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct BulleyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
